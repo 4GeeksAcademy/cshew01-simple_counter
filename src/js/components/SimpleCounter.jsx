6dbb3717602b8348c380c28b4dbd16beeedcbc86
Chris Shewfelt <chris@apromoplace.com>
2024-12-21 01:50:16 +0000

    add a reset button

--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import "../../styles/home.css";
 
+const resetTimer = () => {
+	clearInterval(timer);
+}
+
 const SimpleCounter = ({first, second, third, fourth, fifth}) => {
 	return (
-        <div className="timerContainer">
-			<div className="timerBox">
-				<i className="fa-regular fa-clock"></i>
+        <>
+			<div className="timerContainer">
+				<div className="timerBox">
+					<i className="fa-regular fa-clock"></i>
+				</div>
+				<div className="timerBox">{fifth}</div>
+				<div className="timerBox">{fourth}</div>
+				<div className="timerBox">{third}</div>
+				<div className="timerBox">{second}</div>
+				<div className="timerBox">{first}</div>			
 			</div>
-			<div className="timerBox">{fifth}</div>
-			<div className="timerBox">{fourth}</div>
-			<div className="timerBox">{third}</div>
-			<div className="timerBox">{second}</div>
-			<div className="timerBox">{first}</div>
-		</div>
+			{/* <div className="buttonRow">
+				<button className="start button" onClick="pause=false">Start</button>
+				<button className="stop button" onClick="pause=true">Stop</button>
+				<button className="reset button" onClick="reset=true">Reset</button>
+			</div> */}
+		</>
     )
 }
 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,11 +9,25 @@ import "bootstrap"
 // index.css'
 import '../styles/index.css'
 
+const resetTimer = () => {
+  reset=true;
+  console.log(reset);
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
   let counter=0;
-  
-  setInterval(()=>{
-    
+  let pause=false;
+  let reset=false;
+
+  let timer=setInterval(()=>{
+   
+    if (reset) {
+      counter=0;
+      reset=false;
+    };
+
     const tenThousandsDigit=Math.floor(counter/10000) % 10
     const thousandsDigit=Math.floor(counter/1000) % 10
     const hundredsDigit=Math.floor(counter/100) % 10
@@ -27,6 +41,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
       <React.StrictMode>
         <SimpleCounter first={onesDigit} second={tensDigit} third={hundredsDigit} fourth={thousandsDigit} fifth={tenThousandsDigit}/>
+        <div className="buttonRow">
+          <button className="start button" onClick="pause=false">Start</button>
+          <button className="stop button" onClick="pause=true">Stop</button>
+          <button className="reset button" onClick={resetTimer}>Reset</button>
+        </div>
       </React.StrictMode>,
     )
   },1000)
